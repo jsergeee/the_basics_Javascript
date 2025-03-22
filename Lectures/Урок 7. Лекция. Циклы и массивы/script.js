e79b9b31276bfdde87b92803758ba56c339bcda9
Sergey <jserge@yandex.ru>
2025-03-22 23:49:32 +0300

    22032025

--- Lectures/Урок 7. Лекция. Циклы и массивы/script.js
+++ Lectures/Урок 7. Лекция. Циклы и массивы/script.js
@@ -1,5 +1,7 @@
 // let num = Number(prompt("Введите номер"));
 
+// const { use } = require("express/lib/application");
+
 // let count = 0;
 // while (count < num) {
 //     console.log("Hello!!!");
@@ -34,10 +36,110 @@
 
 // _____________________________________________________
 
-let count = 0;
-alert(`Вы нажали на кнопку ${count} раз`)
-function counterClickButtons() {
-    alert(++count);
-    console.log(count);
+// let count = 0;
+// alert(`Вы нажали на кнопку ${count} раз`)
+// function counterClickButtons() {
+//     alert(++count);
+//     console.log(count);
 
-}
+// }
+//___________Maccивы_________________________
+
+// const students = [];  //  массивы всегда создаются через const!!
+
+// students.push('Иванов');
+// students.push("Петров");
+// students.push("Сидоров");
+// students.push(1);
+
+// console.log(students);
+// console.log(students[0]);
+// console.log(students.length);
+// console.log(students[students.length - 1]);
+
+//_______________
+
+// const user = [];
+// const userName = prompt('Введите Ваше имя');
+// const userAge = prompt("Введите Ваш возраст");
+
+// user.push(userName);
+// user.push(userAge);
+
+// console.log(user);
+//_____________________
+// const user = [];
+
+// user.push(prompt("Введите Ваше имя"));
+// user.push(Number (prompt("Введите Ваше имя")));
+
+// console.log(user);
+
+//_____________Метод pop__________________
+
+// const numbers = [2, 3, 19, 15];
+// alert(`Запомни цифры ${numbers}`)
+
+// // console.log(numbers[numbers.length - 1]); 
+
+// console.log(numbers.pop());
+// console.log(numbers);
+
+
+//______join, split__________________
+
+// const words = 'example'
+// // console.log(words.split(''));
+// const arrWords = words.split('');
+// // console.log(arrWords.pop());
+// const upWord = arrWords.pop();
+// // console.log(upWord.toLocaleUpperCase());
+// // console.log(upWord);
+// // console.log(arrWords.join(','));
+// arrWords.push(upWord.toLocaleUpperCase());
+// console.log(arrWords.join(''));
+
+//_____shift, stack, slise indexOf________________________________
+
+// const arr = [1, 2, 3, 4]
+
+// arr.slice(0,2)
+
+// arr.slice(1,2)
+//_________массивы__________________
+
+// const arr = [5, 1, 2, 3, 7, 19, 7 ,13, 3];
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     console.log(element);
+    
+// }
+
+// for (let i = 0; i < arr.length; i++) {
+//   const element = arr[i];
+//     if (element>=5) {
+//         console.log(element);
+//     } else {
+//         continue;
+//     }
+  
+// }
+
+// console.log(arr.indexOf(3)); 
+
+
+//__________________
+
+const arrAnswers = ['елка', 'ёлка', 'сосна'];
+const userAnswer = prompt('Зимой и летом одним цветом')
+for (let i = 0; i < arrAnswers.length; i++) {
+  const element = arrAnswers[i];
+  if (element === userAnswer) {
+    console.log("Молодец!!!");
+    break;
+  } else {
+    console.log('Не угадал(((');
+    continue;
+  }
+} 
+
---
+++ Seminar/Урок 8. Семинар. Циклы и массивы/script.js
@@ -0,0 +1,264 @@
+//1. Создайте массив с элементами 1, 2, 3. Выведите на экран
+//каждый из этих элементов.
+
+//Создайте массив с произвольными элементами.
+//Выведите на экран количество элементов в этом массиве.
+
+// const array =
+//   "Loremipsumdolorsitametconsecteturadipisicingelit.Temporibus,voluptatesomnisnamdistinctioadipisciquoautemullamtemporeinbeataerecusandaearchitectopariaturvoluptas,oditrepellatnihilmodilaborum?";
+// const arr = array.split("");
+// const arrLength = arr.length;
+// const arrLenghtArray = String(arrLength).split("");
+// const arrLenghtArrayEnding = Number(arrLenghtArray.pop());
+
+// // console.log(arr);
+// // console.log(arr[0]);
+// // console.log(arr[1]);
+// // console.log(arr[2]);
+
+// // for (let i = 0; i < arr.length; i++) {
+// //     console.log(arr[i]);
+
+// // }
+
+// const a = "элемент";
+// const b = "элемента";
+// const c = "элементов";
+
+
+
+// if (arrLenghtArrayEnding === 1) {
+//   ending = a;
+// } else if (arrLenghtArrayEnding >= 2 && arrLenghtArrayEnding <= 4) {
+//   ending = b;
+// } else {
+//   ending = c;
+// } 
+
+
+// console.log(arr.join(", "));
+// console.log(`Длинна массива arr ${arr.length} ${ending}`);
+
+
+// Создайте массив с элементами 'a', 'b', 'c'. Запишите вместо первого элемента число 1, вместо второго - 2, вместо
+//третьего - 3.
+
+// const arr = ['a, b, c'];
+
+// console.log(arr);
+
+// arr[0] = 1;
+// arr[1] = 2;
+// arr[2] = 3;
+
+// console.log(arr);
+
+
+// 1. Создайте массив с элементами 1, 2 и 3. С помощью оператора ++
+// увеличьте каждый элемент массива на единицу.
+
+
+// const arr = [1, 2, 3];
+// arr[0] ++;
+// arr[1] ++;
+// arr[2] ++;
+
+// console.log(arr);
+
+// 2. Узнайте длину следующего массива:
+// const arr = [];
+// arr[3] = 'a';
+// arr[8] = 'b';
+
+// const arr = [];
+// arr[3] = 'a';
+// arr[8] = 'b';
+
+// console.log(arr.length);
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+    
+// }
+
+
+// 2. Пусть дан такой массив:
+// const arr = [1, 2, 3];
+// Добавьте ему в конец элементы 4 и 5.
+
+
+// const arr = [1, 2, 3];
+// arr.push(4)
+// arr.push(5);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+    
+// }
+
+
+
+
+// 4. Создайте произвольный массив из 5 элементов, Удалите из него два
+// элемента. Проверьте, какое станет значение свойства length после
+// этого.
+
+
+// const num = '12345';
+// const arr = num.split('');
+// console.log(`Длинна массива ${arr.length}`);
+// console.log(arr);
+// // for (let i = 0; i < arr.length; i++) {
+// //     console.log(arr[i]);
+    
+// // }
+
+// arr.splice(1,1);
+// console.log(`Длинна массива ${arr.length}`);
+// console.log(arr);
+// // for (let i = 0; i < arr.length; i++) {
+// //   console.log(arr[i]);
+// // }
+
+// arr.splice(0,3);
+// console.log(`Длинна массива ${arr.length}`);
+// console.log(arr);
+
+
+// 1. С помощью цикла for выведите в консоль числа от 11 до 33.
+
+// const array = new Array(100);
+// for (let i = 0; i < array.length; i++) {
+//     array[i] = i;
+//     if (i >= 11 && i <=33) {
+//         // console.log(array[i]++);
+//     } 
+// }
+
+// 2. С помощью цикла for выведите в консоль нечетные числа в
+// промежутке от 1 до 99.
+
+// for (let i = 0; i < array.length; i++) {
+//     array[i] = i;
+//     if (array[i] % 2 !==0) {
+//        console.log(array[i]); 
+//     }
+    
+    
+// }
+
+
+//-----------------------------------------------------------------------
+
+// 3. С помощью цикла for выведите в консоль числа от 100 до 0.
+
+// for (let i = 100; i >= 0; i--) {
+//     console.log(i);
+    
+// }
+
+// 4. Дано число num с неким начальным значением. Умножайте его на 3
+// столько раз, пока результат умножения не станет больше 1000.
+// Какое число получится? Посчитайте количество итераций,
+// необходимых для этого.
+
+// let num = Number(3);
+// let iterations = 0;
+
+// while (num <= 1000) {
+//     num *= 3;
+//     console.log(num);
+//     iterations ++;
+    
+// }
+
+// console.log("Конечное число " + num);
+// console.log("Число итераций " + iterations);
+
+//-------------------------------------------------------------------------
+
+// 1. Дан массив const arr = [2, 5, 9, 15, 1, 4];
+// Выведите в консоль те элементы массива, которые больше 3-х, но
+// меньше 10.
+
+// const arr = [2, 5, 9, 15, 1, 4];
+
+
+// for (let i = 0; i < arr.length; i++) {
+
+//     if(arr[i] > 3 && arr[i] < 10){
+//         console.log(arr[i]);
+//     }
+
+// }
+
+
+
+
+// 1. Найдите сумму четных чисел от 2 до 100.
+
+// let sum = 0;
+// let iterations = 0;
+
+// for (let i = 2; i < 100; i+=2) {
+//     sum += i;
+//     iterations++;
+    
+   
+    
+// }
+// console.log("суммf четных чисел от 2 до 100 равна " + sum + " Кол-во итераций " + iterations);
+
+// 2. Дан массив const = [2, 5, 9, 3, 1, 4];
+// Найдите сумму элементов этого массива.
+
+const arr = [2, 5, 9, 0, 3, 1, 4];
+sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+        
+    }
+console.log(sum);
+
+// 1. С помощью цикла сформируйте строку '-1-2-3-4-5-6-7-8-9-'.
+
+// const arr = new Array(10);
+// result = '';
+// for (let i = 1; i < arr.length; i++) {
+//     result += "-" + i;
+    
+// }
+// result += "-";
+// console.log(result);
+
+
+// 2. Дан массив с числами. const = [2, 5, 9, 0, 3, 1, 4]; Запустите цикл,
+// который будет по очереди выводить элементы этого массива в
+// консоль до тех пор, пока не встретится элемент со значением 0.
+// После этого цикл должен завершить свою работу.
+
+// const arr = [2, 5, 9, 0, 3, 1, 4];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//         console.log(arr[i]);
+//     } else {
+//         break;
+//     }
+    
+// }
+
+
+
+1. Пусть у нас дан массив состоящий из 10 элементов с
+произвольными числами. Давайте переберем его циклом и числа,
+которые делятся на 2, возведем в квадрат и выведем в консоль, а
+числа, которые делятся на 3, возведем в куб и выведем в консоль.
+2. С помощью двух вложенных циклов выведите на экран следующую
+строку:
+3. Дан массив const arr = [1, 2, 3, 2, 4, 3, 5, 6, 3, 2, 3];
+Подсчитайте количество цифр 3 в этом массиве.
+1. Дан следующий массив:
+[1, 2, 3, 4, 5]
+С помощью метода splice преобразуйте массив в следующий:
+[1, 4, 5]
